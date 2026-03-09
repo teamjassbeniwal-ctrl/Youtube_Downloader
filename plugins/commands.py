bfe3b644f9fc8407d188ff60690f706ad7915bed
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-03-09 12:35:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,13 +2,16 @@
 # Repo     : https://github.com/Rexinazor/Youtube_Downloader
 # Author   : Rexinazor
 
-
 from presets import Presets
 from pyrogram.types import Message
 from pyrogram import Client, filters
+from pyrogram.enums import ParseMode  # <-- Add this
 from library.buttons import reply_markup_start
 
 @Client.on_message(filters.private & filters.command(['start', 'help']))
 async def start_bot(bot, m: Message):
-    await m.reply_text(Presets.WELCOME_MSG.format(m.from_user.first_name),
-                       reply_markup=reply_markup_start)
+    await m.reply_text(
+        Presets.WELCOME_MSG.format(m.from_user.first_name),
+        reply_markup=reply_markup_start,
+        parse_mode=ParseMode.HTML  # <-- Fixed parse mode
+    )
